d4418c22754ad08e46b860ef7cefa9c0ae9636e6
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-30 13:34:48 -0300

    Delete src directory mudando pra webpack

--- src/api/billingCycle/billingCycleService.js
+++
@@ -1,6 +0,0 @@
-const billingCycle = require("./billingCycle")
-
-billingCycle.methods(["get", "post", "put", "delete"])
-billingCycle.updateOptions({new: true, runValidator: true})
-
-module.exports = billingCycle
--- src/loader.js
+++
@@ -1,7 +0,0 @@
-const server = require("./config/server")
-require("./config/database")
-
-const routes = require("./config/routes");
-server.use("/api", routes); 
-
-
